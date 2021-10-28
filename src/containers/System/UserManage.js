e84eb07fd22f4bcb43fb8e32343aaa416e36092e
Kien Quan <hotrankienquan144@gmail.com>
2021-10-28 13:25:52 +0700

    video 46

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,74 @@
+import { divide } from 'lodash';
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
 class UserManage extends Component {
-
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
-
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('All');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+            // console.log('check state user 1', this.state.arrUsers);
+        }
+        // console.log(response)
     }
-
+    /**
+     * 
+     * Life cycle
+     * RUn component
+     * 1. run contruct -> init state
+     * 2. did mount (set state)
+     * 3. render
+     * mỗi lần setState thì nó sẽ tự re-render
+     * 
+     */
 
     render() {
+        // console.log('check render', this.state);
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className='title text-center'>manage users with kq</div>
+                <div className='users-table mt-3 mx-1'>
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>FirstName</th>
+                            <th>LastName</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+
+                        {
+                            arrUsers && arrUsers.map((item, index) => {
+                                return (
+                                    <tr>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
+                                            <button className='btn-delete'><i class="far fa-trash-alt"></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+
+
+                    </table>
+                </div>
+            </div>
         );
     }
 
