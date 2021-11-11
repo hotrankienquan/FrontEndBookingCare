04900677aaa762f74702cecd365e7737f0be2ef5
Kien Quan <hotrankienquan144@gmail.com>
2021-11-11 19:49:07 +0700

    hoan tat section bac si noi bat

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -12,15 +12,17 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 class HomePage extends Component {
+	handleAfterChange = () => {
 
+	}
 	render() {
 		let settings = {
 			dots: false,
-			infinite: true,
+			infinite: false,
 			speed: 500,
 			slidesToShow: 4,
 			slidesToScroll: 1,
-
+			afterChange: this.handleAfterChange
 		};
 
 		return (
